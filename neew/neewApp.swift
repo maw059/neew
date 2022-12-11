3fe1b2cde7bb376471d8fc76e1a3287763e787a8
Manal <mw99_a@hotmail.om>
2022-12-11 10:07:44 +0300

    Initial Commit

---
+++ neew/neewApp.swift
@@ -0,0 +1,17 @@
+//
+//  neewApp.swift
+//  neew
+//
+//  Created by Manal alwayeli on 17/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct neewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
